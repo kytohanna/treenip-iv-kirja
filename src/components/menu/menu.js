6ed2659885b19a7e66ca209b6500a23339babd29
kytohanna <kyto.hanna@hotmail.fi>
2021-02-24 10:54:24 +0200

    react router added

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom';
 import styles from './menu.module.scss';
 import ViewListIcon from '@material-ui/icons/ViewList';
 import TimelineIcon from '@material-ui/icons/Timeline';
 import SettingsIcon from '@material-ui/icons/Settings';
-//voit muokkaa ulkonäkö vaihtamalla ikoneja.
+//voit muokkaa ulkonäköä vaihtamalla ikoneja.
 function Menu() {
     return (
       <div className={styles.menu}>
-          <div><ViewListIcon /></div>
-          <div><TimelineIcon /></div>
-          <div><SettingsIcon /></div>
+          <div><Link to="/"><ViewListIcon /></Link></div>
+          <div><Link to="/stats"><TimelineIcon /></Link></div>
+          <div><Link to="/settings"><SettingsIcon /></Link></div>
       </div>
     );
 }
